745eaef8e278fabf8534cab6ac668d33a46e35c9
jungai <juworaphol125@gmail.com>
2021-10-30 19:57:25 +0700

    move js logic to go lol

--- main.go
+++ main.go
@@ -4,17 +4,43 @@ import (
 	"fmt"
 	"os"
 	"os/signal"
+	"regexp"
+	"strconv"
+	"strings"
 	"syscall"
+	"time"
 
 	"github.com/bwmarrin/discordgo"
 )
 
 var (
-	prefix string = "mon"
+	prefix             string = "mon"
+	dropPattern, _            = regexp.Compile(`(^\<\@\d{18}\>) is dropping \d cards!$`)
+	grabPattern, _            = regexp.Compile(`(^\<\@\d{18}\>) took the \*\*.*\*\* card .\*!`)
+	dailyPattern, _           = regexp.Compile(`you earned a daily reward`)
+	purchasePattern, _        = regexp.Compile(`(^\<\@\d{18}\>), please follow this link to complete your purchase`)
+	checkIsDigit, _           = regexp.Compile(`^[0-9]+$`)
 )
 
+var (
+	simpleDontBuyMessage = "อย่าเติมเลยค้าบบ 😅"
+	hardDontBuyMessage   = "ก็บอกว่าอย่าเติมมม 😡"
+	dropMessage          = "**Drop** currently available 😗"
+	grabMessage          = "**Grab** currently available 😉"
+	dailyMessage1        = "see ya for next daily is available 🥳"
+	dailyMessage2        = "**Daily** currently available 😉"
+)
+
+func getUser(id string) string {
+	return fmt.Sprintf("<@%v>", id)
+}
+
+func isValidPrefix(p string) bool {
+	return p == prefix
+}
+
 func main() {
-	discord, err := discordgo.New("Bot " + "token")
+	discord, err := discordgo.New("Bot " + os.Getenv("TOKEN"))
 
 	if err != nil {
 		fmt.Println("Cannot connect to server")
@@ -44,4 +70,111 @@ func main() {
 	discord.Close()
 }
 
-func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {}
+func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
+	// for karuta bot here
+	switch {
+	case dropPattern.MatchString(m.Content):
+		time.Sleep(time.Minute * 30)
+		s.ChannelMessageSend(m.ChannelID, fmt.Sprintf("%v %v", getUser(m.Author.ID), dropMessage))
+
+		return
+	case grabPattern.MatchString(m.Content):
+		time.Sleep(time.Minute * 30)
+		s.ChannelMessageSend(m.ChannelID, fmt.Sprintf("%v %v", getUser(m.Author.ID), grabMessage))
+
+		return
+	case dailyPattern.MatchString(m.Content):
+		s.ChannelMessageSend(m.ChannelID, dailyMessage1)
+		time.Sleep(time.Second * 84_600)
+		s.ChannelMessageSend(m.ChannelID, fmt.Sprintf("%v %v", getUser(m.Author.ID), dailyMessage2))
+
+		return
+	case purchasePattern.MatchString(m.Content):
+		s.ChannelMessageDelete(m.ChannelID, m.ID)
+		s.ChannelMessageSend(m.ChannelID, hardDontBuyMessage)
+
+		return
+	}
+
+	// Check Embed
+	if len(m.Embeds) != 0 {
+		switch {
+		case m.Embeds[0].Title == "Purchase Gems":
+			s.ChannelMessageDelete(m.ChannelID, m.ID)
+			s.ChannelMessageSend(m.ChannelID, simpleDontBuyMessage)
+
+			return
+		}
+	}
+
+	// user command
+	rawMonCommand := strings.Split(m.Content, " ")
+
+	monCommand := make([]string, 4)
+	copy(monCommand, rawMonCommand)
+
+	prefix, command, value1, value2 := monCommand[0], monCommand[1], monCommand[2], monCommand[3]
+
+	if !isValidPrefix(prefix) {
+		return
+	}
+	// check prefix
+
+	switch command {
+	case "cd":
+		s.ChannelMessageSend(m.ChannelID, fmt.Sprintf("%v see ya in 30 min", getUser(m.Author.ID)))
+		time.Sleep(time.Second * 3)
+		s.ChannelMessageSend(m.ChannelID, fmt.Sprintf("%v already 30 min 😏", getUser(m.Author.ID)))
+
+		return
+
+	case "cg":
+		s.ChannelMessageSend(m.ChannelID, fmt.Sprintf("%v see ya in 10 min", getUser(m.Author.ID)))
+		time.Sleep(time.Second * 2)
+		s.ChannelMessageSend(m.ChannelID, fmt.Sprintf("%v already 10 min 😏", getUser(m.Author.ID)))
+
+		return
+
+	case "vi":
+		s.ChannelMessageSend(m.ChannelID, fmt.Sprintf("%v see ya in 2 hours", getUser(m.Author.ID)))
+		time.Sleep(time.Hour * 2)
+		s.ChannelMessageSend(m.ChannelID, fmt.Sprintf("%v vi kub", getUser(m.Author.ID)))
+
+		return
+
+	case "count":
+		if condi := strings.Split(value1, "hr"); checkIsDigit.MatchString(condi[0]) && len(condi) == 2 {
+			s.ChannelMessageSend(m.ChannelID, fmt.Sprintf("%v see ya in %v hours", getUser(m.Author.ID), condi[0]))
+			realNumber, _ := strconv.ParseInt(condi[0], 10, 32)
+			time.Sleep(time.Hour * time.Duration(realNumber))
+
+			if value2 == "" {
+				s.ChannelMessageSend(m.ChannelID, fmt.Sprintf("%v already %v hours 😏", getUser(m.Author.ID), value1))
+				return
+			}
+
+			s.ChannelMessageSend(m.ChannelID, fmt.Sprintf("%v -> %v", getUser(m.Author.ID), value2))
+
+			return
+
+		}
+
+		if checkIsDigit.MatchString(value1) {
+			s.ChannelMessageSend(m.ChannelID, fmt.Sprintf("%v see ya in %v min", getUser(m.Author.ID), value1))
+			realNumber, _ := strconv.ParseInt(value1, 10, 32)
+			time.Sleep(time.Minute * time.Duration(realNumber))
+
+			if value2 == "" {
+				s.ChannelMessageSend(m.ChannelID, fmt.Sprintf("%v already %v min 😏", getUser(m.Author.ID), value1))
+
+				return
+			}
+
+			s.ChannelMessageSend(m.ChannelID, fmt.Sprintf("%v -> %v", getUser(m.Author.ID), value2))
+
+			return
+
+		}
+	}
+
+}
